0d1ffa2540bd5016cb31647545b9d22fec1f4e72
freelife <freejava1191@gmail.com>
2019-01-06 19:36:41 +0900

    location comment

--- src/main/java/me/freelife/rest/events/Event.java
+++ src/main/java/me/freelife/rest/events/Event.java
@@ -14,7 +14,7 @@ public class Event {
     private LocalDateTime beginEnrollmentDateTime; //등록 시작일시
     private LocalDateTime closeEnrollmentDateTime; //종료일시    private LocalDateTime beginEventDateTime; //이벤트 시작일시
     private LocalDateTime endEventDateTime;   //이벤트 종료일시
-    private String location; // (optional) 이게 없으면 온라인 모
+    private String location; // (optional) 이벤트 위치 이게 없으면 온라인 모임
     private int basePrice; // (optional)
     private int maxPrice; // (optional)
     private int limitOfEnrollment; //등록한도
