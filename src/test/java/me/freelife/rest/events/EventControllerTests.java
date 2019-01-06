5e4b7a568351332987ed19ba21d84b2779920cfb
freelife <freejava1191@gmail.com>
2019-01-07 01:06:28 +0900

    chap02-05 fail on unknown properties

--- src/test/java/me/freelife/rest/events/EventControllerTests.java
+++ src/test/java/me/freelife/rest/events/EventControllerTests.java
@@ -33,8 +33,39 @@ public class EventControllerTests {
     @Autowired
     ObjectMapper objectMapper;
 
-    @Test
+    @Test //EventDto를 사용하여 값을 만든경우 잘동작함
     public void createEvent() throws Exception {
+        EventDto event = EventDto.builder()
+                .name("Spring")
+                .description("REST API Development with Spring")
+                .beginEnrollmentDateTime(LocalDateTime.of(2018, 11, 23, 14, 21))
+                .closeEnrollmentDateTime(LocalDateTime.of(2018, 11, 24, 14, 21))
+                .beginEventDateTime(LocalDateTime.of(2018, 11, 25, 14, 21))
+                .endEventDateTime(LocalDateTime.of(2018, 11, 26, 14, 21))
+                .basePrice(100)
+                .maxPrice(200)
+                .limitOfEnrollment(100)
+                .location("강남역 D2 스타텁 팩토리")
+                .build();
+
+        mockMvc.perform(post("/api/events/")
+                    .contentType(MediaType.APPLICATION_JSON_UTF8) //요청타입
+                    .accept(MediaTypes.HAL_JSON) //받고싶은 타입
+                    .content(objectMapper.writeValueAsString(event))) //event를 json을 String으로 맵핑
+                .andDo(print())
+                .andExpect(status().isCreated()) // 201 상태인지 확인
+                .andExpect(jsonPath("id").exists()) //ID가 있는지 확인
+                .andExpect(header().exists(HttpHeaders.LOCATION)) // HEADER에 Location 있는지 확인
+                .andExpect(header().string(HttpHeaders.CONTENT_TYPE, MediaTypes.HAL_JSON_UTF8_VALUE)) //Content-Type 값 확인
+                .andExpect(jsonPath("id").value(Matchers.not(100))) // ID가 100이 아니면
+                .andExpect(jsonPath("free").value(Matchers.not(true))) // free가 true가
+                .andExpect(jsonPath("eventStatus").value(EventStatus.DRAFT.name()))
+
+        ;
+    }
+
+    @Test // Event를 사용해서 이상한 값들을 넣어주면 응답이 isBadRequest로 나오길 바람
+    public void createEvent_Bad_Request() throws Exception {
         Event event = Event.builder()
                 .id(100)
                 .name("Spring")
@@ -53,18 +84,11 @@ public class EventControllerTests {
                 .build();
 
         mockMvc.perform(post("/api/events/")
-                    .contentType(MediaType.APPLICATION_JSON_UTF8) //요청타입
-                    .accept(MediaTypes.HAL_JSON) //받고싶은 타입
-                    .content(objectMapper.writeValueAsString(event))) //event를 json을 String으로 맵핑
+                .contentType(MediaType.APPLICATION_JSON_UTF8) //요청타입
+                .accept(MediaTypes.HAL_JSON) //받고싶은 타입
+                .content(objectMapper.writeValueAsString(event))) //event를 json을 String으로 맵핑
                 .andDo(print())
-                .andExpect(status().isCreated()) // 201 상태인지 확인
-                .andExpect(jsonPath("id").exists()) //ID가 있는지 확인
-                .andExpect(header().exists(HttpHeaders.LOCATION)) // HEADER에 Location 있는지 확인
-                .andExpect(header().string(HttpHeaders.CONTENT_TYPE, MediaTypes.HAL_JSON_UTF8_VALUE)) //Content-Type 값 확인
-                .andExpect(jsonPath("id").value(Matchers.not(100))) // ID가 100이 아니면
-                .andExpect(jsonPath("free").value(Matchers.not(true))) // free가 true가
-                .andExpect(jsonPath("eventStatus").value(EventStatus.DRAFT.name()))
-
+                .andExpect(status().isBadRequest()) // 400 Bad Request
         ;
     }
 
