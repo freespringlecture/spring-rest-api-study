1256c8f7fa56986dd88653a98ad4ec8ee428b1a9
freelife <freejava1191@gmail.com>
2019-01-07 17:35:38 +0900

    chap03-07 Detahch Settings form TestDB

--- src/test/java/me/freelife/rest/events/EventControllerTests.java
+++ src/test/java/me/freelife/rest/events/EventControllerTests.java
@@ -14,6 +14,7 @@ import org.springframework.context.annotation.Import;
 import org.springframework.hateoas.MediaTypes;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -33,6 +34,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @AutoConfigureMockMvc
 @AutoConfigureRestDocs
 @Import(RestDocsConfiguration.class)
+@ActiveProfiles("test")
 public class EventControllerTests {
 
     @Autowired
