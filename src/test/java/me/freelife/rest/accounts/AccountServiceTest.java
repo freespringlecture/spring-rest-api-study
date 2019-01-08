a7b81f7e17fb536b10950b66cba22624ec039ccc
freelife <freejava1191@gmail.com>
2019-01-08 12:07:17 +0900

    chap05-03 Exception Test Three Type

--- src/test/java/me/freelife/rest/accounts/AccountServiceTest.java
+++ src/test/java/me/freelife/rest/accounts/AccountServiceTest.java
@@ -1,23 +1,31 @@
 package me.freelife.rest.accounts;
 
 import me.freelife.rest.common.TestDescription;
+import org.hamcrest.Matchers;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.Set;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.fail;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @ActiveProfiles("test")
 public class AccountServiceTest {
+
+    @Rule
+    public ExpectedException expectedException = ExpectedException.none();
 
     @Autowired
     AccountService accountService;
@@ -45,4 +53,36 @@ public class AccountServiceTest {
         // Then
         assertThat(userDetails.getPassword()).isEqualTo(password);
     }
+
+    @Test(expected = UsernameNotFoundException.class)
+    @TestDescription("없는 사용자의 경우 UsernameNotFoundException 예외 발생")
+    public void findByUsernameFail_ExceptionType() {
+        String username = "random@email.com";
+        accountService.loadUserByUsername(username);
+    }
+
+    @Test
+    @TestDescription("없는 사용자의 경우 예외 타입과 메세지 확인")
+    public void findByUsernameFail_ExceptionTypeMessage() {
+        String username = "random@email.com";
+        try {
+            accountService.loadUserByUsername(username);
+            fail("supposed to be failed");
+        } catch (UsernameNotFoundException e) {
+            assertThat(e.getMessage()).containsSequence(username);
+        }
+    }
+
+    @Test
+    @TestDescription("없는 사용자의 경우 예상되는 예외로 타입과 메세지 확인")
+    public void findByUsernameFail() {
+        // Expected
+        String username = "random@email.com";
+        expectedException.expect(UsernameNotFoundException.class);
+        expectedException.expectMessage(Matchers.containsString(username));
+
+        // When
+        accountService.loadUserByUsername(username);
+    }
+
 }
